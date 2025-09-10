5c707b6d988891fb88e8f95ef8424619d93d02fa
Silviu Barbu <barbu.silviu92@gmail.com>
2025-09-10 20:49:33 +0300

    Again first commit

---
+++ Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+    public static void main(String a[]) {
+        int num = 20;
+        byte b = 122;
+        short sh = 5882;
+        long l = 22222222223l;
+        float f = 23.2f;
+
+        char c = 'k';
+        boolean ba = true;
+    }
+}
