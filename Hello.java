107a4cb6925f79949192d3d75d343d4f558e33b6
Silviu Barbu <barbu.silviu92@gmail.com>
2025-09-24 20:26:38 +0300

    Lesson 2

--- Hello.java
+++ Hello.java
@@ -1,12 +1,14 @@
 public class Hello {
     public static void main(String a[]) {
-        int num = 20;
-        byte b = 122;
-        short sh = 5882;
-        long l = 22222222223l;
-        float f = 23.2f;
+        // int num = 20;
+        // byte b = 122;
+        // short sh = 5882;
+        // long l = 22222222223l;
+        // float f = 23.2f;
 
-        char c = 'k';
-        boolean ba = true;
+        // char c = 'k';
+        // boolean ba = true;
+        int num = 100_000_000;
+        System.out.println(num);
     }
 }
