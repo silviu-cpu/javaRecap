e36c5eb998af6b8b8ba0d9b40e413630a1d93e35
Silviu Barbu <barbu.silviu92@gmail.com>
2025-10-18 11:39:24 +0300

    Lesson 8

--- Hello.java
+++ Hello.java
@@ -1,61 +1,33 @@
-class Mobile {
-    String brand;
-    int price;
-    String network;
-    static String name;
+class Human {
+    private int age = 22;
+    private String name = "Silvik";
 
-    static {
-        name = "Phone";
-        System.out.println("in static block");
+    public Human(int age, String name) {
+        this.age = age;
+        this.name = name;
     }
 
-    public Mobile() {
-        brand = " ";
-        price = 200;
-        System.out.println("In constructor");
+    public int getAge() {
+        return age;
     }
 
-    public void show() {
-        System.out.println(brand + ": " + price + ": " + name);
+    public String getName() {
+        return name;
     }
 
-    public static void show1(Mobile obj) {
-        System.out.println("In static method: " + obj.brand + ": " + obj.price + ": " + name);
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
 
 public class Hello {
     public static void main(String a[]) {
-        // String name = new String("Silvik");
-        // String s1 = "Silvik";
-        // String s2 = "Silvik";
+        Human obj = new Human(83, "Ilie");
 
-        // System.out.println(s1.hashCode());
-        // System.out.println(s2.hashCode());
-
-        // StringBuffer sb = new StringBuffer("Silviu");
-        // System.out.println(sb.capacity());
-        // System.out.println(sb.length());
-        // sb.append(" Barbu");
-        // sb.deleteCharAt(2);
-        // sb.insert(0, "Java ");
-
-        // String str = sb.substring(0, 5);
-        // System.out.println(str);
-        Mobile obj1 = new Mobile();
-        obj1.brand = "Apple";
-        obj1.price = 1500;
-        Mobile.name = "StaticCeva";
-
-        Mobile obj2 = new Mobile();
-        obj2.brand = "Samsung";
-        obj2.price = 2500;
-
-        // Mobile.name = "TePhone";
-
-        obj1.show();
-        obj2.show();
-
-        Mobile.show1(obj1);
+        System.out.println(obj.getAge() + " " + obj.getName());
     }
 }
