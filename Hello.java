b3a6c539afeab18900f3aa472a89f943f5f61439
Silviu Barbu <barbu.silviu92@gmail.com>
2025-10-18 12:00:46 +0300

    Lesson 10

--- Hello.java
+++ Hello.java
@@ -3,25 +3,13 @@ class A {
         System.out.println("Constr in A");
     }
 
-    public A(int m) {
-        System.out.println("Constr in A int m");
-    }
-}
-
-class B extends A {
-    public B() {
-        super();
-        System.out.println("Constr in B");
-    }
-
-    public B(int n) {
-        this(); // will call the constructor of the class B()
-        System.out.println("Constr in B int n");
+    public void show() {
+        System.out.println("Show in A");
     }
 }
 
 public class Hello {
     public static void main(String a[]) {
-        B obj = new B(6);
+        new A().show(); // anonymous object has no refference
     }
 }
