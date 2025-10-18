16fc4069bb973413561742f9e84ef511c3a49c99
Silviu Barbu <barbu.silviu92@gmail.com>
2025-10-18 11:58:16 +0300

    Lesson 9

--- Hello.java
+++ Hello.java
@@ -1,33 +1,27 @@
-class Human {
-    private int age = 22;
-    private String name = "Silvik";
-
-    public Human(int age, String name) {
-        this.age = age;
-        this.name = name;
+class A {
+    public A() {
+        System.out.println("Constr in A");
     }
 
-    public int getAge() {
-        return age;
+    public A(int m) {
+        System.out.println("Constr in A int m");
+    }
+}
+
+class B extends A {
+    public B() {
+        super();
+        System.out.println("Constr in B");
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public void setName(String name) {
-        this.name = name;
+    public B(int n) {
+        this(); // will call the constructor of the class B()
+        System.out.println("Constr in B int n");
     }
 }
 
 public class Hello {
     public static void main(String a[]) {
-        Human obj = new Human(83, "Ilie");
-
-        System.out.println(obj.getAge() + " " + obj.getName());
+        B obj = new B(6);
     }
 }
